806e507c94ef6ebe33a7523978d2fbcee6f3d14d
RayZh <rayzhangshanghai@hotmail.com>
2025-05-02 10:42:48 +0800

    feat: finish src.cpp (entrypoint for code)

--- src.cpp
+++ src.cpp
@@ -1,5 +1,39 @@
 #include "b_plus_tree.hpp"
 
+using norb::hash::fnv1a_hash;
+using norb::hash::hashed_t_;
+
 int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  norb::BPlusTree<hashed_t_, int> tree;
+
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; i++) {
+    std::string mode, index;
+    int value;
+    std::cin >> mode >> index;
+    const auto hashed_index = fnv1a_hash(index);
+    if (mode == "insert") {
+      std::cin >> value;
+      tree.insert(hashed_index, value);
+    } else if (mode == "delete") {
+      std::cin >> value;
+      tree.remove(hashed_index, value);
+    } else {
+      bool found_any = false;
+      tree.find_all_do(hashed_index, [&found_any](const int &x) {
+        std::cout << x << ' ';
+        found_any = true;
+      });
+      if (!found_any) {
+        std::cout << "null";
+      }
+      std::cout << '\n';
+    }
+  }
   return 0;
 }
